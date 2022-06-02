b015b0fc127012d698dc978f9e3d4e75b6ffbd0a
Andrey-Pank <99199092+Andrey-Pank@users.noreply.github.com>
2022-06-02 19:29:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -9,7 +9,7 @@ std::vector<char> getPerm(const Tree& tree, int n) {
   std::string out = tree[n -1];
   std::vector<char> res;
   for (unsigned int step = 0; step < out.size(); step++) {
-      res.push_back(out[i]);
+      res.push_back(out[step]);
   }
   return res;
 }
